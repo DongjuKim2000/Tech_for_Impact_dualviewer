69d9765395fb0a2466a643f69b2be856869c453e
Dongju <skipper080837@gmail.com>
2023-10-04 22:50:57 +0900

    runnable 객체 줄였음, ui delay 부분 삭제, 향후 ui,ux 조사에서 요구사항 생길 시 추가 요망
    show, hide, toggle 작동 최대한 간단히

--- app/src/main/java/com/example/dualviewer/FullscreenActivity1_DJ.kt
+++ app/src/main/java/com/example/dualviewer/FullscreenActivity1_DJ.kt
@@ -33,40 +33,15 @@ class FullscreenActivitydj : AppCompatActivity() {
     val showinfobr = ShowinfoBR()
     var activityName = "액티비티1"
 
-
-    //  lateinit로 textview, linechart등 객체 선언
-    // onCreate 에서 각각 다 정의됨
-    // fullscreen 안에서 각 view를 나눠서 섹션이 만들어져 있음
-    // 레이아웃 간소화 시킬 수 있을듯?
     private lateinit var fullscreenContent5: LinearLayout
 
     // UI 작업용, handler로 작업을 큐에 넣음, null 아닐 때 hidehanlder 실행시키기
     private val hideHandler = Handler(Looper.myLooper()!!)
 
-    @SuppressLint("InlinedApi")
-    private val hidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
-        if (Build.VERSION.SDK_INT >= 30) {
-            fullscreenContent5.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-        } else {
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            fullscreenContent5.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        }
-    }
 
-    // 나중에 actionbar delay 시켜서 표시하기 위해 runnable로 정의해놓음, 이게 필요가 있나 확인해보기
-    private val showPart2Runnable = Runnable {
-        // Delayed display of UI elements
-        supportActionBar?.show()
-        //fullscreenContentControls.visibility = View.VISIBLE
-    }
 
     // fullscreen인지 아닌지, 초기는 false -> ui가 안보이는 상태로 만들어짐(toggle 부분 확인)
     private var isFullscreen: Boolean = false
-
-    private val hideRunnable = Runnable { hide() }
 
     // broadcastreceivnfoer에서 메시지를 수신할 때 호출됨
     //    // intent.action이 showi 이면 실행시키는데 어떨때 showinfo 인지 확인해야될듯
@@ -77,22 +52,6 @@ class FullscreenActivitydj : AppCompatActivity() {
                 showinfo()
         }
     }
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-//    private val delayHideTouchListener = View.OnTouchListener { view, motionEvent ->
-//        when (motionEvent.action) {
-//            MotionEvent.ACTION_DOWN -> if (AUTO_HIDE) {
-//                delayedHide(AUTO_HIDE_DELAY_MILLIS)
-//            }
-//            MotionEvent.ACTION_UP -> view.performClick()
-//            else -> {
-//            }
-//        }
-//        false
-//    }
 
     @SuppressLint("ClickableViewAccessibility")
     // 메뉴 표시를 위해서 작성된거, xml로 변환 가능한데 닥스 참고
@@ -241,55 +200,38 @@ class FullscreenActivitydj : AppCompatActivity() {
         // Hide UI first
         supportActionBar?.hide()
         //fullscreenContentControls.visibility = View.GONE
-        isFullscreen = false
-
         // Schedule a runnable to remove the status and navigation bar after a delay
-        hideHandler.removeCallbacks(showPart2Runnable)
-        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
-
+        hideHandler.removeCallbacks(hidePart2Runnable)
+        hideHandler.post(hidePart2Runnable)
+        isFullscreen = false
     }
 
     private fun show() {
 
         // Show the system bar
-        if (Build.VERSION.SDK_INT >= 30) {
-            fullscreenContent5.windowInsetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-
-        } else {
-            fullscreenContent5.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        }
-
-        isFullscreen = true
-
+        supportActionBar?.show()
         // Schedule a runnable to display UI elements after a delay
-        hideHandler.removeCallbacks(hidePart2Runnable)
-        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY.toLong())
+        hideHandler.removeCallbacks(showPart2Runnable)
+        hideHandler.post(showPart2Runnable)
+        isFullscreen = true
 
     }
 
+    @SuppressLint("InlinedApi")
+    private val hidePart2Runnable = Runnable {
+        if (Build.VERSION.SDK_INT >= 30) {
+            fullscreenContent5.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            fullscreenContent5.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+    }
 
-// 아래 2개는 처음 터치를 이용해 ui 변경이 가능하다는 것을 인식 시키려고 들어가 있는데 오히려 지저분해서 빼는게 더 좋을듯
-//
-// 시간 지나면 hide 시키려는 것 같은데 왜 굳이? 그냥 터치로 조작하는게 더 직관적일듯
-// 아래에서 호출되는 형태임.
-// 근데 작동을 안하는디?
-//    private fun delayedHide(delayMillis: Int) {
-//        hideHandler.removeCallbacks(hideRunnable)
-//        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
-//    }
-
-    // 최초 onCreate() 호출 이후 호출되는 메서드
-//    override fun onPostCreate(savedInstanceState: Bundle?) {
-//        super.onPostCreate(savedInstanceState) // 필수 구성요소
-//
-//        // Trigger the initial hide() shortly after the activity has been
-//        // created, to briefly hint to the user that UI controls
-//        // are available.
-//        //log.d(activityName,"onPostCreate")
-//
-//        // 여기서 delayedHide가 호출된 상태임
-//        delayedHide(10)
-//    }
+    private val showPart2Runnable = Runnable {
+        supportActionBar?.show()
+    }
 
 
     private fun showinfo() {
